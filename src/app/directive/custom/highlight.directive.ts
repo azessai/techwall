59832fa0157fdfe69a98140bec86c5d77dc8fc7a
ES SAI <azeddine.essai@gmail.com>
2021-12-06 09:05:05 +0100

    Directive personalisée

---
+++ src/app/directive/custom/highlight.directive.ts
@@ -0,0 +1,15 @@
+import {Directive, HostBinding, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  @HostBinding('style.backgroundColor') bg ='red';
+  constructor() { }
+  @HostListener('mouseenter') mouseenter() {
+    this.bg = 'yellow';
+  }
+  @HostListener('mouseleave') mouseleave() {
+    this.bg = 'red';
+  }
+}
